6df290d8a132e00112d6de9ad19effcc8719a6e2
= <=>
2019-11-04 18:14:14 -0500

    homework2

--- unit-2/homework/hw-6.py
+++ unit-2/homework/hw-6.py
@@ -1,8 +1,10 @@
-marvel_movies = ['Captain Marvel', 'Avengers Game', 'The Averngers','Iron Man 3', 'DeadPool', 'Ant-man', 'Thor-The Dark World', 'Black Panther','SpiderMan-Homecoming','Guardians of the Galaxy']
+marvel_movies = ['Captain Marvel', 'Avengers Game', 'The Averngers','Iron Man 3', 'DeadPool', 'Ant-man', 'Thor-The Dark World', 'Black Panther','SpiderMan-Homecoming','Guardians of The Galaxy']
 
-special_marvel_movies = ''
+special_marvel_movies = []
 
-for special_title in marvel_movies:
-    special_marvel_movies = special_title.append('the')
+for movie_title in marvel_movies:
+    if 'The' in movie_title:
+        special_marvel_movies.append(movie_title)
 
-special_marvel_movies = special_marvel_movies.add() 
+print(special_marvel_movies)
+
--- unit-2/homework/hw-7.py
+++ unit-2/homework/hw-7.py
@@ -5,9 +5,14 @@ import random
 name = input('Enter your name: ')
 random_message = ['You are awesome', 'you are a party animal', 'you are crazy', 'you need to focus', 'time to sleep', 'you are a winner', 'are you hungry']
 
+while name != 'e':
+    print(name +'!', random.choice(random_message))
+    name = input('Enter your name: ')
 
+    if name == 'e':
+        print('End of Game')
   
 
 
-print(name +'!', random.choice(random_message))
+
 
